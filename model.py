ba833c3e44bbf0d705f90a127011a4a8b481c78c
ryosuke <rogerrafarn0324@gmail.com>
2025-05-19 21:50:05 +0900

    .

--- model.py
+++ model.py
@@ -20,7 +20,8 @@ def scrape_openwork(email: str, password: str, base_url: str, headless: bool = T
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
     options.add_argument('--lang=ja-JP')
-    options.binary_location = os.environ.get('CHROME_BIN','/usr/bin/chromium-browser')
+
+    options.binary_location = os.environ.get('CHROME_BIN', '/usr/bin/chromium')
 
     driver = webdriver.Chrome(
         service=ChromeService(ChromeDriverManager().install()),
